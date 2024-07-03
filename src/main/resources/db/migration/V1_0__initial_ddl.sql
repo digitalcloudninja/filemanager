1bcc1299ad9b221fb94b7e6715b0e8be04db6329
DigitalCloud Ninja <admin@digitalcloud.ninja>
2024-07-02 20:18:31 -0600

    Updating build

--- src/main/resources/db/migration/V1_0__initial_ddl.sql
+++ src/main/resources/db/migration/V1_0__initial_ddl.sql
@@ -1,18 +1,7 @@
-create extension if not exists "uuid-ossp";
-
-create function filemanager.random_uuid()
-    returns uuid
-    language plpgsql
-as
-$$
-begin
-    return filemanager.uuid_generate_v4();
-end;
-$$;
-
+create schema if not exists "filemanager";
 create table filemanager.files
 (
-    id   uuid default filemanager.random_uuid() not null,
+    id   uuid default public.random_uuid() not null,
     data oid,
     name varchar(255),
     size bigint,
